d81b497f241356233e3f3a797c105413eb62e06f
Olha Khomyn <olha@Olhas-MacBook-Pro.local>
2023-02-09 22:30:03 +0100

    changes rates

--- message_formatter/src/lib.rs
+++ message_formatter/src/lib.rs
@@ -2,9 +2,9 @@ use serde::{Serialize, Deserialize};
 use serde_json::to_string;
 
 pub enum Operation {
-    TraderCapital(f32, f32, f32, f32),  // eur, usd, yuan, yen
-    SellBuyRates(Vec<(String, f32, f32, f32, f32)>), // market_name, eur, usd, yuan, yen
-    SellBuyTransaction(SellBuy, String, Currency, f32), // type of operation, market name, currency, amount
+    TraderCapital(f64, f64, f64, f64),  // eur, usd, yuan, yen
+    SellBuyRates(Vec<(String, (f64, f64), (f64, f64), (f64, f64), (f64, f64))>), // market_name, eur, usd, yuan, yen
+    SellBuyTransaction(SellBuy, String, Currency, f64), // type of operation, market name, currency, amount
     Other(String),
 }
 
@@ -23,42 +23,43 @@ pub enum Currency {
 }
 
 #[derive(Serialize, Deserialize)]
-struct TraderCapitalStruct {
-    eur: f32,
-    usd: f32,
-    yuan: f32,
-    yen: f32,
+pub struct TraderCapitalStruct {
+    pub eur: f64,
+    pub usd: f64,
+    pub yuan: f64,
+    pub yen: f64,
 }
 
 #[derive(Serialize, Deserialize)]
-struct SellBuyRatesStruct {
-    name: String,
-    eur: f32,
-    usd: f32,
-    yen: f32,
-    yuan: f32,
+pub struct SellBuyRatesStruct {
+    pub name: String,
+    //sell,buy
+    pub eur: (f64, f64),
+    pub usd: (f64, f64),
+    pub yen: (f64, f64),
+    pub yuan: (f64, f64),
 }
 
 #[derive(Serialize, Deserialize)]
-struct SellBuyRatesVec {
-    v: Vec<SellBuyRatesStruct>,
+pub struct SellBuyRatesVec {
+    pub v: Vec<SellBuyRatesStruct>,
 }
 
 #[derive(Serialize, Deserialize)]
-struct SellBuyTransactionStruct {
-    transaction: SellBuy,
-    name: String,
-    currency: Currency,
-    amount: f32,
+pub struct SellBuyTransactionStruct {
+    pub transaction: SellBuy,
+    pub name: String,
+    pub currency: Currency,
+    pub amount: f64,
 }
 
 #[derive(Serialize, Deserialize)]
 struct OtherStruct {
-    s: String,
+    pub s: String,
 }
 
 
-fn create_trader_capital(eur: f32, usd: f32, yuan: f32, yen: f32) {
+pub fn create_trader_capital(eur: f64, usd: f64, yuan: f64, yen: f64) {
     let structure = TraderCapitalStruct {
         eur,
         usd,
@@ -72,7 +73,7 @@ fn create_trader_capital(eur: f32, usd: f32, yuan: f32, yen: f32) {
     }
 }
 
-fn create_sell_buy_rates(name: String, eur: f32, usd: f32, yuan: f32, yen: f32) -> SellBuyRatesStruct {
+pub fn create_sell_buy_rates(name: String, eur: (f64, f64), usd: (f64, f64), yuan: (f64, f64), yen: (f64, f64)) -> SellBuyRatesStruct {
     SellBuyRatesStruct {
         name,
         eur,
@@ -82,7 +83,7 @@ fn create_sell_buy_rates(name: String, eur: f32, usd: f32, yuan: f32, yen: f32) 
     }
 }
 
-fn create_sell_buy_rates_vec(v_tuple: Vec<(String, f32, f32, f32, f32)>) {
+fn create_sell_buy_rates_vec(v_tuple: Vec<(String, (f64, f64), (f64, f64), (f64, f64), (f64, f64))>) {
     let mut structure = SellBuyRatesVec {
         v: Vec::new(),
     };
@@ -98,7 +99,7 @@ fn create_sell_buy_rates_vec(v_tuple: Vec<(String, f32, f32, f32, f32)>) {
     }
 }
 
-fn create_sell_buy_transaction(transaction: SellBuy, name: String, currency: Currency, amount: f32) {
+fn create_sell_buy_transaction(transaction: SellBuy, name: String, currency: Currency, amount: f64) {
     let structure = SellBuyTransactionStruct {
         transaction,
         name,
@@ -130,5 +131,3 @@ pub fn send_message(op: Operation) {
         Operation::Other(s) => create_other(s),
     }
 }
-
-
